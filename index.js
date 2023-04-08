43f046c780d20ecb51f52736cf4faa84098c19be
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-04-08 11:35:52 -0700

    week 1 node,js up and running

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+const http = require("http"); 
+http.createServer((req,res) => {
+    var path = req.url.toLowerCase();    
+    switch(path) {
+        case '/':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Welcome to my Home page for IT 122! To learn a bit more about me visit localhost:3000/about');
+            break;
+        case '/about':
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+            res.end('Welcome to my About page! I have been studying web design now for two years. I am excited to continue learning javascript!');
+            break;
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not found');
+            break;
+    }    
+}).listen(process.env.PORT || 3000);
