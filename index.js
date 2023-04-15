a32e224825088ad8a95ac8137555fc5714203c39
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-04-15 10:40:22 -0700

    add more data/ some css

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ import express from 'express';
 import * as data from './public/data.js';
 
 const app = express();
-app.use(express.urlencoded()); //Parse URL-encoded bodies
 app.use(express.json()); //Used to parse JSON bodies
 
 
 // configure path to public folder which will store all static files such as images and stylesheets
-import * as path from 'path';
+//import * as path from 'path';
 app.use(express.static('./public'));
 
 // setup view engine, we will be using ejs
@@ -39,6 +38,13 @@ app.get('/details/:title', (req, res) => {
         result
         }
     );
+});
+
+// define 404 handler
+app.use((req,res) => {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Oops! page Not found');
 });
 
 // setup server to run on port 3000 (localhost:3000)
--- public/data.js
+++ public/data.js
@@ -1,9 +1,9 @@
 const movies = [
-    {"title": "Psycho", "director": "Alfred Hitchcock", "yearReleased": 1960},
-    {"title": "The Exorcist", "director": "William Friedkin", "yearReleased": 1963},
-    {"title": "The Shining", "director": "Stanley Kubrick", "yearReleased": 1980},
-    {"title": "Creature From the Black Lagoon", "director": "Jack Arnold", "yearReleased": 1954},
-    {"title": "The Blob", "director": "Irvin Yeaworth", "yearReleased": 1958}
+    {"title": "Psycho", "director": "Alfred Hitchcock", "yearReleased": 1960, "budget": "$806,947", "boxOffice": "$50 million"},
+    {"title": "The Exorcist", "director": "William Friedkin", "yearReleased": 1963, "budget": "$12 million", "boxOffice": "$441.3 million"},
+    {"title": "The Shining", "director": "Stanley Kubrick", "yearReleased": 1980, "budget": "$19 million", "boxOffice": "$47.3 million"},
+    {"title": "Creature From the Black Lagoon", "director": "Jack Arnold", "yearReleased": 1954, "budget": "~$500,000", "boxOffice": "$1.3 million"},
+    {"title": "The Blob", "director": "Irvin Yeaworth", "yearReleased": 1958, "budget": "$110,000", "boxOffice": "$4 million"}
 ];
 
 const getAll = () => {
