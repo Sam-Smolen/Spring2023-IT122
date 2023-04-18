0dc2a8e1f36db52d5dd67c73d8a1568f03c9260b
Sam-Smolen <87501744+Sam-Smolen@users.noreply.github.com>
2023-04-18 13:09:25 -0700

    app connects/pulls data from mongoDb

--- models/Movie.js
+++ models/Movie.js
@@ -1,3 +1,29 @@
 // importing data from mongoDb movies collection
 
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
 
+// For security, connectionString should be in a separate file and excluded from git
+const connectionString = "mongodb+srv://samsmolen_schooldb_user:Basilisk312@cluster0.wv0dqox.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(connectionString, {
+    dbName: 'school_projects_db',
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('open', () => {
+  console.log('Mongoose connected.');
+});
+
+// define data model as JSON key/value pairs
+// values indicate the data type of each key
+const movieSchema = new Schema({
+ title: { type: String, required: true },
+ director: String,
+ yearReleased: Number,    // must be JS data type
+ budget: String,
+ boxOffice: String
+});
+
+export const Movie = mongoose.model('Movie', movieSchema);
--- test_db.js
+++ test_db.js
@@ -1,1 +1,10 @@
 // testing mongodb connection
+
+import { Movie } from "./models/Movie.js";
+
+// return all records
+Movie.find({}).lean()
+  .then((movies) => {
+    console.log(movies);
+  })
+  .catch(err => next(err));
